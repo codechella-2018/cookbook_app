cffd501205f1a3a380f8b06e23df89458cc1037f
dani <dani.zaghian@gmail.com>
2018-07-03 20:07:19 -0700

    added axios request for recipe data

--- public/index.js
+++ public/index.js
@@ -1,9 +1,19 @@
 var recipeTemplate = document.querySelector('#recipe-card');
 var recipeRow = document.querySelector(".row");
 
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
-recipeRow.appendChild(recipeTemplate.content.cloneNode(true));
+/* global axios */
+axios.get("http://localhost:3000/api/recipes").then(function(response){
+	var recipes = response.data;
+	console.log(recipes);
+
+	// for each recipe, create a recipeTemplate card with that recipes data
+	recipes.forEach(function(recipe){
+		var recipeClone = recipeTemplate.content.cloneNode(true);
+		recipeClone.querySelector(".card-img-top").src = recipe.image_url;
+		recipeClone.querySelector(".card-title").innerText = recipe.title;
+		recipeClone.querySelector(".chef").innerText = recipe.chef_name;
+		recipeClone.querySelector(".prep-time").innerText = recipe.formatted.prep_time;
+		recipeClone.querySelector(".ingredients").innerText = recipe.formatted.ingredients;
+		recipeRow.appendChild(recipeClone);
+	});
+})
