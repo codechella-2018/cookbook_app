f179c8a65336405b0942bd8c445427c573772312
dani <dani.zaghian@gmail.com>
2018-07-10 18:29:12 -0700

    made a vuejs sample page

--- public/index.js
+++ public/index.js
@@ -4,7 +4,6 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
       recipes: []
     };
   },
@@ -18,8 +17,23 @@ var HomePage = {
   computed: {}
 };
 
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "Welcome to the sample page"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+  	{ path: "/", component: HomePage }, 
+  	{ path: "/sample", component: SamplePage}
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
